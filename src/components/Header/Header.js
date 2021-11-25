f0e731d998c49e6b7858f84e5fa76738fff62acb
Mohammad815 <mohammadforhad176@gmail.com>
2021-11-25 12:28:07 +0600

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
         <div className=' mb-1'>
             <div className="card bg-primary text-white mb-3">
                 <h3 className="card-text">Make Book Store App</h3>
-                <p>Everyone can Choose their favorite book and collected from this book fair Site</p>
-                <h3 className="card-text mb-2">Total Budget: 100 Million </h3>
+                <p>Everyone can Choose their favorite book and collected from this book fair Site.</p>
+                <h3 className="card-text mb-2">Total Budget: 100 Million Dollars </h3>
             </div>
         </div>
        
